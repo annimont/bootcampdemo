27e29ada7dc8819602ece56a5b3bb58181035627
annimont <anni.monttinen@gmail.com>
2020-04-07 20:24:23 +0300

    lista järjestetty id:n mukaan

--- backend/networth.js
+++ backend/networth.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 const getNetworth = (req, res) =>
-    db.any('select * from networth')
+    db.any('select * from networth order by id')
     .then(listFromDb => res.send(listFromDb))
     .catch(error => res.status(500).send(error))
 
